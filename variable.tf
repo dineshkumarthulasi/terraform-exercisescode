56b2c25f0953f21468c5d307f91417860b5fdada
dineshkumarthulasi <145987933+dineshkumarthulasi@users.noreply.github.com>
2024-07-16 19:06:17 +0530

    Update variable.tf

---
+++ variable.tf
@@ -0,0 +1,27 @@
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "db_username" {
+  description = "The username for the RDS instance"
+  type        = string
+  default     = "admin"
+}
+
+variable "db_password" {
+  description = "The password for the RDS instance"
+  type        = string
+  default     = "yourpassword"  # Change this to a secure password
+  sensitive   = true
+}
