a29ad1917d522f87659357fd7c0a5bb4d9cc0e24
dineshkumarthulasi <145987933+dineshkumarthulasi@users.noreply.github.com>
2024-07-16 19:06:37 +0530

    Update output.tf

---
+++ output.tf
@@ -0,0 +1,24 @@
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  description = "IDs of public subnets"
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "IDs of private subnets"
+  value       = aws_subnet.private[*].id
+}
+
+output "alb_dns_name" {
+  description = "DNS name of the Application Load Balancer"
+  value       = aws_lb.application_lb.dns_name
+}
+
+# output "db_endpoint" {
+#   description = "Endpoint of the MySQL Database"
+#   value       = aws_db_instance.mysql_db.endpoint
+# }
